c775492d094f739f5c766741aac4e7d28a06b81f
PikachuGabe <84477180+PikachuGabe@users.noreply.github.com>
2025-01-31 15:03:41 -0600

    Initial commit of Gradle configuration

---
+++ src/main/java/com/pika/bcm/bcm.java
@@ -0,0 +1,13 @@
+package com.pika.bcm;
+
+import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(bcm.ID)
+public class bcm
+{
+    // Define mod id in a common place for everything to reference
+    public static final String ID = "bcm";
+    public static final String NAME = "BeansCraftLib";
+    public static final String VERSION = "1.0.0";
+}
